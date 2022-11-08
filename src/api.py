d87105ca7984ed3e81803a3ec536ec3df20907f3
itc-s22026 <104543147+itc-s22026@users.noreply.github.com>
2022-11-08 15:55:56 +0900

    Update api.py

--- src/api.py
+++ src/api.py
@@ -1,10 +1,13 @@
+from curses.ascii import TAB
 import requests, json
-url = "https://zipcloud.ibsnet.co.jp/api/search"
 while True:
-    a = input("郵便番号を入力してください:")
-    break
-url = url + "?zipcode={0}".format(a)
-res = requests.get(url)
-data = json.loads(res.content)
-area = data["results"][0]
-print(area["address1"] + area["address2"] + area["address3"])
+        name = input('ポケモンの名前を入力してください。:')
+        uri = f'https://pokeapi.co/api/v2/pokemon/{name}/'
+        res = requests.get(uri)
+        an = json.loads(res.content)
+        print(an['abilities'][0]['ability']['name'])
+        #print(an['abilities'][0]['ability']['url'])
+        print(an['abilities'][1]['ability']['name'])
+        #print(an['abilities'][1]['ability']['url'])
+        if (res := input('他のポケモンも調べますか？ y/n') != 'y'):
+                break
